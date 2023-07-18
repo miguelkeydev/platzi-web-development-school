b2d3983fa9ba9777ffd3b094f36a77f38f910537
miguelkeydev <miguelsuarezm14@gmail.com>
2023-07-18 14:04:05 -0500

    Learning about 'Values"

---
+++ courses/javascript-basic/classes/values.js
@@ -0,0 +1,19 @@
+// Number
+10, 20, 14, 12, 21
+
+// String
+"Miguel, a future web developer"
+
+// Boolean
+true, false
+
+// Empty
+null, undefined
+
+// Array
+[1, 2, 3, "Four", false]
+
+// Object
+{
+  name: 'Josue'
+}
