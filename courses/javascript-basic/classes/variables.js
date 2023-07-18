cc57029ca9f07290f35b8564ff6e9aeba6dfa6e2
miguelkeydev <miguelsuarezm14@gmail.com>
2023-07-18 14:27:59 -0500

    Learning 'Variables'

---
+++ courses/javascript-basic/classes/variables.js
@@ -0,0 +1,14 @@
+var name = "Miguel";
+
+// Declaration
+var age;
+
+// Inicialization
+age = 19;
+
+var elements = ["Computer", "Phone", "Tablet"];
+
+var person = {
+  name: "Miguel",
+  age: 30
+};
