fd146ef3bfe60fd6395f39bea6adb09e9f8d2700
miguelkeydev <miguelsuarezm14@gmail.com>
2023-07-18 15:59:40 -0500

    Learning 'Scope'

---
+++ courses/javascript-basic/classes/scope.js
@@ -0,0 +1,12 @@
+let myName = "Camila";
+
+function fullName() {
+  let myLastName = "Ramos";
+  console.log(`Hi ${myName} ${myLastName}`);
+}
+
+fullName(); // Hi Camila Ramos
+
+myName; // Camila
+
+myLastName // Error: It's not defined
