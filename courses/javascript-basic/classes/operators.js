328a48caa165f3b30e5dd1ba509f90417ab5e9ec
miguelkeydev <miguelsuarezm14@gmail.com>
2023-07-19 09:18:38 -0500

    Learning 'Operators'

---
+++ courses/javascript-basic/classes/operators.js
@@ -0,0 +1,55 @@
+// Assignment
+let assignment = "Assign";
+
+// Sum
+let a = 3 + 5; // 8
+
+// Substraction
+let b = 5 - 3; // 2
+
+// Multiplication
+let c = 5 * 3; // 15
+
+// Division
+let d = 15 / 3; // 5
+
+// Concatenation
+let concatenation = "Pedrito" + "is amazing";
+
+// False
+let falseOperator = !false; // true
+
+// Value Comparation
+3 == "3"; // true
+
+// Type and Value Comparation
+71 === "71" // false
+
+// Smaller Than
+9 < 7; // false
+
+// Greater Than
+9 > 7; // true
+
+// Greater or Smaller or Equal Than
+7 >= 11; // false
+8 <= 8; // true
+
+// Not the same
+2 != 4; // true
+
+// Only if all elements are true
+a && b && c;
+
+// Or, If any of the elements is true, it's true
+a || b || c || d;
+
+// To increase one number
+let age = 35;
+age++; // 36
+
+// To Increase any number
+age += 8; // 44
+
+// To Substract any number
+age -= 14; // 30
