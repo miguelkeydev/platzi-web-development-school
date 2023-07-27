fd160cf5e88846f2f957a7cf20965521db7f4752
miguelkeydev <miguelsuarezm14@gmail.com>
2023-07-27 14:57:23 -0500

    Learning about 'Constructor Function'

--- courses/javascript-basic/classes/objects.js
+++ courses/javascript-basic/classes/objects.js
@@ -7,3 +7,15 @@ let car = {
     console.log(`Model ${this.model} ${this.year}`);
   }
 };
+
+// Constructor
+function computer(cpu, ram, graphics, storage) {
+  this.cpu = cpu;
+  this.ram = ram;
+  this.graphics = graphics;
+  this.storage = storage;
+}
+
+// Creating an object from the Constructor Function
+let newComputer = new computer("Ryzen 7", "16GB", "RTX 3080", "2 TB");
+let newLaptot = new computer("Intel i5", "8GB", "Radeon Vega 2020", "1 TB");
